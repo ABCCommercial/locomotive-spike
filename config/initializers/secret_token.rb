e19226f93df29774070b41af79c66fe6ec797f8e
Harish <harishreddyreddy@gmail.com>
2012-01-13 09:56:26 +1100

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocomotiveSpike::Application.config.secret_token = '1d960cd18f503303c0537b3fee45b1e09b60568d641778f55888de0478071f0f7b8409f4ff94cd9bb55e4115f960d1d5718b3507f1f68b8995dadbacd94efa7c'
